32991ee59576e597445e8b8ea3306251eecaa632
naneri <ktnaneri@gmail.com>
2020-09-06 12:15:43 +0600

    added npm and composer and updated .sh code and also created a storage for db data

--- deploy/up.sh
+++ deploy/up.sh
@@ -3,3 +3,6 @@
 # set folder permissions
 sudo chgrp -R www-data ../storage ../bootstrap/cache
 sudo chmod -R ug+rwx ../storage ../bootstrap/cache
+
+docker-compose run --rm composer install
+docker-compose run --rm npm install
